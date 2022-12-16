25a7b39730580a4e58cb3c07419fb72d624f8e49
firsttoken <monikasharma12330@gmail.com>
2022-12-16 16:57:55 +0530

    Q3_Table_powerof2

---
+++ Q3_Table_powerof2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Q3_Table_powerof2
+{
+    public static void tableofpoweroftwo()
+    {
+        Console.WriteLine("Enter the value for n to show the table nth term: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+
+        if (n >= 0 && n < 31)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                double expr = Math.Pow(2, j);
+
+                Console.WriteLine("2^" + j + "= " + expr);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input!!!");
+        }
+    }
+}
