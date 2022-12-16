c40f9eba4ca7dfb237c78b53fb0a2095a480af6c
firsttoken <monikasharma12330@gmail.com>
2022-12-16 16:46:15 +0530

    Main_Method_of_Project_in_it.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        //Day3---Practice-1
+            Q1_Flip_coin.flipcoin();
+                Console.WriteLine("\n");
+            Q2_Leep_year.leapyear();
+                Console.WriteLine("\n");
+            Q3_Table_powerof2.tableofpoweroftwo();
+                Console.WriteLine("\n");
+            Q4_Harmonic_No.Harmonicno();
+                Console.WriteLine("\n");
+            Q5_Prime_Factor.primefactor();
+                Console.WriteLine("\n");
+        //Day7---Practice-2
+            Q6_Quotient_Reminder.Quotient_and_Reminder_check();
+                Console.WriteLine("\n");
+            Q7_SwapValues.Swap_Numbers();
+                Console.WriteLine("\n");
+            Q8_EnvenorOdd.Check_EvenOdd();
+                Console.WriteLine("\n");
+            Q9_VowalorConsonant.Checkchar_VorC();
+                Console.WriteLine("\n");
+            Q10_GreatestNum.Check_greatestno();
+                Console.WriteLine("\n");
+
+
+
+        Console.ReadKey(true);
+    }
+}
