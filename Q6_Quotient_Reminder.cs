33fb4d7fa44e8db5b1902532667e57d4408a4c0e
firsttoken <monikasharma12330@gmail.com>
2022-12-16 17:51:40 +0530

    Q6_Quotient_Reminder

---
+++ Q6_Quotient_Reminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Q6_Quotient_Reminder
+{
+	public static void Quotient_and_Reminder_check()
+	{
+		int divident = 50;
+		int divisor = 10;
+		int Quotient = divident / divisor;
+		int Reminder= divident % divisor;
+
+
+		Console.WriteLine("divident {0} divisor {1} ", divident, divisor);
+
+        Console.WriteLine("Quotient = " + Quotient);
+        Console.WriteLine("Reminder = "+Reminder);
+
+
+    }
+}
