60285ac8138cd7515b30a5401f82d265489afaab
firsttoken <monikasharma12330@gmail.com>
2022-12-16 18:02:04 +0530

    Q10_Find_Greatest_Num

---
+++ Q10_GreatestNum.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+
+public class Q10_GreatestNum
+{
+	public static void Check_greatestno()
+	{
+        Console.WriteLine("Lets check the largest number among three.");
+		Console.WriteLine("Enter the Number 1:");
+        int n1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Number 2:");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Number 3:");
+        int n3 = Convert.ToInt32(Console.ReadLine());
+
+        if(n1>n2)
+        {
+            if(n1>n3)
+            {
+                Console.WriteLine("Number 1 is Greatest.");
+            }
+            else
+            {
+                Console.WriteLine("Number 3 is Greatest.");
+            }
+        }
+        else if(n2>n3)
+        {
+            Console.WriteLine("Number 2 is Greatest.");
+        }
+        else
+        {
+            Console.WriteLine("Number 3 is Greatest.");
+        }
+    }
+}
