0a61191580194b94aaeb6895ffa92b46106d5a6c
firsttoken <monikasharma12330@gmail.com>
2022-12-16 16:55:53 +0530

    Q2_Leep_year

---
+++ Q2_Leep_year.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Q2_Leep_year
+{
+    public static void leapyear()
+    {
+        int check = 0;
+        int check1 = 0;
+        Console.WriteLine("Enter the year to know, Is it leap year or not: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        if (year > 1000 && year < 9999)
+        {
+            check = year % 400;
+            check1 = year % 4;
+            if (check == 0 && check1 == 0)
+            {
+                Console.WriteLine("Entered year is a leep year.");
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not a leap year.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input.");
+        }
+    }
+}
