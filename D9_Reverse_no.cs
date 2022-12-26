908ce018cf3b9addf8df489fc2d28b207a6485dd
firsttoken <monikasharma12330@gmail.com>
2022-12-26 20:03:22 +0530

    Reverse no

---
+++ D9_Reverse_no.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class D9_Reverse_no
+{
+	public static void Get_Reverse()
+	{
+		Console.WriteLine("Enter the number to reverse : ");
+		int num=Convert.ToInt32(Console.ReadLine());
+		int rem;
+		int reverse = 0;
+		
+
+		while (num != 0) 
+		{
+			rem = num % 10;
+			reverse = reverse*10 + rem;
+			num= num / 10;
+
+		}
+		Console.WriteLine("Reversed number is: " + reverse);
+	}
+}
